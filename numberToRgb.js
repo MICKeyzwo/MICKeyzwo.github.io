8e0fb15d2c9cd542a9b9d244c3f1ee4b6e679a37
MICKeyzwo <singing.programmer@gmail.com>
2017-05-04 22:09:34 +0900

    number to rgb

---
+++ numberToRgb.js
@@ -0,0 +1,26 @@
+﻿function getRgb(n, s){
+    if(typeof s === "string"){
+        s = "rgb".split("").indexOf(s);
+        if(s === -1) return;
+    }
+    if(s >= 0 && s <= 2){
+        if(typeof n === "string"){
+            if(n.indexOf("#") != -1){
+                n = parseInt(n.replace("#", ""), 16);
+            }else{
+                n = parseInt(n, 16);
+            }
+        }
+        if(s == 0){
+            return n >> 16;
+        }else if(s == 1){
+            return n >> 8 & 0xff;
+        }else if(s == 2){
+            return n & 0xff;
+        }else{
+            return;
+        }
+    }else{
+        return;
+    }
+}
