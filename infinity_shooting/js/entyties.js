b7bb240776038c1b33570ef0e9041b69a364b487
MICKeyzwo <singing.programmer@gmail.com>
2017-07-01 02:59:24 +0900

    remove infinity shooting

--- infinity_shooting/js/entyties.js
+++
@@ -1,3 +0,0 @@
-window._setEntyties = function(source){
-    
-}
--- infinity_shooting/js/scriptGetter.js
+++
@@ -1,6 +0,0 @@
-window.getScript = function(src){
-    let script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = src;
-    document.body.appendChild(script).parentNode.removeChild(script);
-}
